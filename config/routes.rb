65aad1569b9f6a02a3b24ff0903688bcc0e0cb9a
Jeremy Walker <jeremy@meducation.net>
2013-08-05 18:44:30 +0100

    Add more job info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Company::Application.routes.draw do
   get "about" => "pages#about"
-  get "jobs" => "pages#jobs"
+
+  get "jobs" => "jobs#index"
+  get "designer_job" => "jobs#designer"
+  get "frontend_developer_job" => "jobs#frontend_developer"
+  get "backend_developer_job" => "jobs#backend_developer"
+  get "full_stack_developer_job" => "jobs#full_stack_developer"
+  get "community_manager_job" => "jobs#community_manager"
+  get "content_manager_job" => "jobs#content_manager"
+  get "marketer_job" => "jobs#marketer"
+
   root "pages#index"
 end
