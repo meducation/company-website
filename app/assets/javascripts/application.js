f27f585e470205130a37a6a687955b19439f7197
Jeremy Walker <jeremy@meducation.net>
2013-08-06 04:01:00 +0100

    Added some styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require ../../../vendor/assets/javascripts/jquery.min.js
+//= require ../../../vendor/assets/javascripts/modernizr.custom.js
+//= require ../../../vendor/assets/javascripts/bootstrap.min.js
+//= require ../../../vendor/assets/javascripts/jquery.flexslider.min.js
+//= require ../../../vendor/assets/javascripts/jquery.cslider.js
+//= require ../../../vendor/assets/javascripts/app.js
+//= require ../../../vendor/assets/javascripts/index.js
 //= require_tree .
