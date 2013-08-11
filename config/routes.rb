58ee538f70013018c6bd057ff386ddde849e1e58
Jeremy Walker <jeremy@meducation.net>
2013-08-11 16:52:57 +0100

    Add job spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Company::Application.routes.draw do
   get "vision" => "pages#vision"
 
   get "jobs" => "jobs#index"
-  get "designer_job" => "jobs#designer"
-  get "frontend_developer_job" => "jobs#frontend_developer"
-  get "backend_developer_job" => "jobs#backend_developer"
-  get "full_stack_developer_job" => "jobs#full_stack_developer"
-  get "community_manager_job" => "jobs#community_manager"
-  get "content_manager_job" => "jobs#content_manager"
-  get "marketer_job" => "jobs#marketer"
+  get "jobs/principles" => "jobs#principles", as: "job_principles"
+  get "jobs/designer" => "jobs#designer", as: "designer_job"
+  get "jobs/frontend_developer" => "jobs#frontend_developer", as: "frontend_developer_job"
+  get "jobs/backend_developer" => "jobs#backend_developer", as: "backend_developer_job"
+  get "jobs/full_stack_developer" => "jobs#full_stack_developer", as: "full_stack_developer_job"
+  get "jobs/community_manager" => "jobs#community_manager", as: "community_manager_job"
+  get "jobs/content_manager" => "jobs#content_manager", as: "content_manager_job"
+  get "jobs/marketer" => "jobs#marketer", as: "marketer_job"
 
   get "blog" => "blog#index"
   %w{rob_styles new_website}.each do |title|
