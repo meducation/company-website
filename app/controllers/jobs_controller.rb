75577a2736a27414ab63b26dbf2993faaa585b35
Jeremy Walker <jeremy@meducation.net>
2013-08-12 01:08:53 +0100

    Refactor jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,20 @@
 class JobsController < ApplicationController
+  before_action :set_jobs
+
   def index
     set_page_title "Jobs at Meducation"
   end
+  
+  private
+    def set_jobs
+      @jobs = {
+        #"Designer" => designer_job_path,
+        #"Frontend Developer" => frontend_developer_job_path,
+        #"Backend Developer" => backend_developer_job_path,
+        #"Full Stack Developer" => full_stack_developer_job_path,
+        "Community Manager" => community_manager_job_path,
+        "Content Manager" => content_manager_job_path,
+        #"Marketer" => marketer_job_path
+      }
+    end
 end
---
+++ spec/requests/general_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe "Check everything loads" do
+  pages = %w{
+    root about investors vision 
+    blog rob_styles_blog_post new_website_blog_post
+    jobs job_principles
+    backend_developer_job
+    community_manager_job content_manager_job
+    designer_job
+    frontend_developer_job full_stack_developer_job
+    marketer_job
+  }
+  pages.each do |page|
+    it "{page}_url should load" do
+      get send("#{page}_url")
+      response.response_code.should == 200
+    end
+  end
+end
