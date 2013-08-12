d8ba5b7c85edb115b5dcc25bc437f3ad6c6b2352
Jeremy Walker <jeremy@meducation.net>
2013-08-12 01:38:26 +0100

    Add ror and javascript developer pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,12 +8,11 @@ class JobsController < ApplicationController
   private
     def set_jobs
       @jobs = {
-        #"Designer" => designer_job_path,
-        #"Frontend Developer" => frontend_developer_job_path,
-        #"Backend Developer" => backend_developer_job_path,
-        #"Full Stack Developer" => full_stack_developer_job_path,
         "Community Manager" => community_manager_job_path,
         "Content Manager" => content_manager_job_path,
+        #"Designer" => designer_job_path,
+        "JavaScript Developer" => javascript_developer_job_path,
+        "Ruby on Rails Developer" => ror_developer_job_path,
         #"Marketer" => marketer_job_path
       }
     end
--- spec/requests/general_spec.rb
+++ spec/requests/general_spec.rb
@@ -5,11 +5,11 @@ describe "Check everything loads" do
     root about investors vision 
     blog rob_styles_blog_post new_website_blog_post
     jobs job_principles
-    backend_developer_job
     community_manager_job content_manager_job
     designer_job
-    frontend_developer_job full_stack_developer_job
+    javascript_developer_job
     marketer_job
+    ror_developer_job
   }
   pages.each do |page|
     it "{page}_url should load" do
