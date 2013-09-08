68210bbcadc078589dee8bf51294990301d1f8f2
Jeremy Walker <jeremy@meducation.net>
2013-09-09 00:56:46 +0100

    Correctly order blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,7 +1,7 @@
 class BlogPostsController < ApplicationController
   def index
     set_page_title "Meducation Blog"
-    @blog_posts = BlogPost.all
+    @blog_posts = BlogPost.order("created_at DESC")
   end
 
   def show
