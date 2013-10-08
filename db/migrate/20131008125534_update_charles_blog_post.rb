b47150c19cfe2417f1e2e9a88d000dd4398c5ba0
Jeremy Walker <jez.walker@gmail.com>
2013-10-08 16:47:57 +0100

    Fix weird search error

--- db/migrate/20131008125534_update_charles_blog_post.rb
+++ db/migrate/20131008125534_update_charles_blog_post.rb
@@ -1,6 +1,6 @@
 class UpdateCharlesBlogPost < ActiveRecord::Migration
   def up
-    blogPost = BlogPost.friendly.find("Charles Care joins Meducation")
+    blogPost = BlogPost.where(title: "Charles Care joins Meducation").first
     blogPost.summary = "Charles Care joins Meducation as a developer"
     blogPost.save()
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910225338) do
+ActiveRecord::Schema.define(version: 20131008125534) do
 
   create_table "blog_posts", force: true do |t|
     t.string   "title"
